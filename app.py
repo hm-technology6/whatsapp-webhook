8dc4b60a191c9a8c5a26d12c228610f5e783f832
hm-technology6 <sdsdsbsg@gmail.com>
2025-05-30 18:40:25 +0400

    Update app.py

--- app.py
+++ app.py
@@ -20,4 +20,6 @@ def webhook():
         return "Event received", 200
 
 if __name__ == "__main__":
-    app.run()
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
